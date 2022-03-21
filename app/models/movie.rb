3964298273823edb7083a45114351c08d4a4aa5c
Zelie Chowaiki <zeliechowaiki@gmail.com>
2022-03-21 10:25:30 -0400

    commit tree 6a10254acf39ffc066be233bbf5f1a3b7a00bea0

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,49 @@
 class Movie < ActiveRecord::Base
 
+  def self.create_with_title(title)
+    new_movie = Movie.new(title: title)
+    new_movie.save
+    new_movie
+  end
+
+  def self.first_movie
+    Movie.first
+  end
+
+  def self.last_movie
+    Movie.last
+  end
+
+  def self.movie_count
+    Movie.count
+  end
+
+  def self.find_movie_with_id(id)
+    Movie.find(id)
+  end
+
+  def self.find_movie_with_attributes(attributes)
+    Movie.find_by(attributes)
+  end
+
+  def self.find_movies_after_2002
+    Movie.where("release_date > 2002")
+  end
+
+  def update_with_attributes(attributes)
+    self.update(attributes)
+  end
+
+  def self.update_all_titles(title)
+    Movie.update(title: title)
+  end
+
+  def self.delete_by_id(id)
+    Movie.destroy(id)
+  end
+
+  def self.delete_all_movies
+    Movie.destroy_all
+  end
+
 end
